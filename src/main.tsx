297e5a63297fb5c488bf1884d0aa11a43d6f28bd
ucnhtmenow-oss <ucnhtmenow@gmail.com>
2026-07-19 04:01:23 -0700

    Enable Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error("Root element was not found.");
+  throw new Error("Root element not found.");
 }
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
